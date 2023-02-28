282d17308af5e0123250fd4096e8570f60fea566
Gabriela <gabriela.lewenfus@saltpay.co>
2023-02-28 13:29:59 +0000

    core: add tbleau authenticator class

--- tableau-ext/tableau_ext/extension.py
+++ tableau-ext/tableau_ext/extension.py
@@ -6,6 +6,7 @@ import pkgutil
 import subprocess
 import sys
 from pathlib import Path
+import requests
 from typing import Any
 
 import structlog
@@ -13,8 +14,10 @@ from meltano.edk import models
 from meltano.edk.extension import ExtensionBase
 from meltano.edk.process import Invoker, log_subprocess_error
 
+from tableau_auth import TableauAuth
 log = structlog.get_logger()
 
+ENV_PREFIX = "TABLEAU_"
 
 class Tableau(ExtensionBase):
     """Extension implementing the ExtensionBase interface."""
@@ -23,6 +26,9 @@ class Tableau(ExtensionBase):
         """Initialize the extension."""
         self.tableau_bin = "tableau"  # verify this is the correct name
         self.tableau_invoker = Invoker(self.tableau_bin)
+        authenticator = TableauAuth()
+        authenticator.sign_in()
+        self.tableau_headers = authenticator.get_headers()
 
     def invoke(self, command_name: str | None, *command_args: Any) -> None:
         """Invoke the underlying cli, that is being wrapped by this extension.
