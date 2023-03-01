aa714b656989b243a089d9739a5fd765ba67a1bf
Pedro Lopes <phslop@gmail.com>
2023-03-01 14:31:35 +0000

    refactor

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,24 @@
 import os
-from unittest.mock import patch
-import unittest
+from unittest import TestCase, mock
 
 from tableau_ext.utils import prepared_env
 
-class TestUtils(unittest.TestCase):
+
+class TestUtils(TestCase):
     def test_prepared_env(self) -> None:
         envs = {
-            'TEST_TOKEN_SECRET': 'a',
-            'TEST_TOKEN_NAME': 'b',
-            'TEST_BASE_URL': 'c',
-            'TEST_API_VERSION': 'd',
-            'TEST_SITE_ID': 'e',
+            "TEST_TOKEN_SECRET": "a",
+            "TEST_TOKEN_NAME": "b",
+            "TEST_BASE_URL": "c",
+            "TEST_API_VERSION": "d",
+            "TEST_SITE_ID": "e",
         }
 
-        with unittest.mock.patch.dict(os.environ, envs):
+        with mock.patch.dict(os.environ, envs):
             config = prepared_env("TEST")
 
             assert len(config) == 5
-            assert config["TOKEN_SECRET"] == 'a'
-        
-        with unittest.mock.patch.dict(os.environ, {}) and self.assertRaises(Exception):
+            assert config["TOKEN_SECRET"] == "a"
+
+        with mock.patch.dict(os.environ, {}) and self.assertRaises(Exception):
             config = prepared_env("TEST")
